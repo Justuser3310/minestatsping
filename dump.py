3516559260e6319f4f311a0d0a578066281de22d
Void User <justuser31>
2023-01-14 23:47:19 +0300

    Add server

--- dump.py
+++ dump.py
@@ -1,16 +1,36 @@
+#Json
 import json
 def read():
         global db
         with open('db.json', 'r') as openfile:
                 db = json.load(openfile)
 
-read()
 
-for i in db:
-	ttime = db[i]
 
-	hours = ttime//60//60 ; ttime = ttime - hours*60*60
-	minutes = ttime//60 ; ttime = ttime - minutes*60
-	seconds = ttime
+def dump():
+	read()
+	for i in db:
+		ttime = db[i]
 
-	print(f'{i} >> {hours}:{minutes}:{seconds}')
+		hours = ttime//60//60 ; ttime = ttime - hours*60*60
+		minutes = ttime//60 ; ttime = ttime - minutes*60
+		seconds = ttime
+
+		return f'{i[:i.find("[")]} >> {hours}:{minutes}:{seconds}'
+
+
+
+#Simple server
+import tornado.httpserver, tornado.ioloop, tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(dump())
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+if __name__ == "__main__":
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(8888)
+    tornado.ioloop.IOLoop.instance().start() 
+
